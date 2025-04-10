e84e6cb48155c989c04a7cc28abd43ab82aca37a
anshukumarbth <anshukumarbth1@gmail.com>
2025-04-11 04:27:31 +0530

    update api

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NewsItems from './components/NewsItems'
 
 function App() {
+  
   return (
     <div className='container ml-auto mr-auto'>
       <NewsItems/>
--- src/components/NewsItems.jsx
+++ src/components/NewsItems.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NewsBox from "./NewsBox";
 import { useState, useEffect } from "react";
+import { FaMicrophone } from "react-icons/fa";
 function NewsItems() {
   const [isOpen, setOpen] = useState(false);
   const [response, setResponse] = useState([]);
@@ -10,71 +11,81 @@ function NewsItems() {
     const fetchNews = async () => {
       try {
         const res = await fetch(
-          `https://newsdata.io/api/1/latest?apikey=pub_7947310a754800ac4a6a275c685fd0bbe0492&q=${apidata}`
+          `https://newsapi.org/v2/everything?q=${apidata}&apiKey=c841617635e943d4bff6eba7beeaeba3`
         );
         const data = await res.json();
-        console.log(data.results); // Now this will work as expected
-        setResponse(data.results);
+        console.log(data.articles); // Now this will work as expected
+        setResponse(data.articles);
       } catch (error) {
         console.error("Error fetching news:", error);
       }
     };
 
     fetchNews();
-  }, [apidata,query]);
+  }, [apidata, query]);
 
   const handlechange = () => {
     setApiData(query);
     setQuery("");
-  }
+  };
+
+  let speechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+  let recognition = new speechRecognition(0);
+  recognition.onresult = (e) => {
+    let currentIndex = e.resultIndex;
+    let transscript = e.results[currentIndex][0].transcript;
+    console.log(transscript);
+    setApiData(transscript);
+  };
 
   return (
     <>
-      <div className="w-full bg-black fixed top-0 left-0 z-50 text-white">
+      <div className="fixed top-0 left-0 z-50 w-full text-white bg-black">
         {/* Desktop Navbar */}
-        <div className="hidden md:flex justify-center items-center h-20">
-          <div className="text-2xl font-mono text-center bg-red-700 rounded-full px-4 py-2 hover:bg-red-600 hover:tracking-wider max-lg:text-xl transition-all duration-300 shadow-lg shadow-yellow-500">
+        <div className="items-center justify-center hidden h-20 md:flex">
+          <div className="px-4 py-2 font-mono text-2xl text-center transition-all duration-300 bg-red-700 rounded-full shadow-lg hover:bg-red-600 hover:tracking-wider max-lg:text-xl shadow-yellow-500">
             <h1 className="animate-bounce">NewSApp</h1>
           </div>
-          <ul className="flex flex-row justify-center items-center px-10 py-4 gap-15 text-xl font-mono font-bold max-lg:gap-2 max-lg:px-1 max-xl:px-5 max-xl:gap-5">
+          <ul className="flex flex-row items-center justify-center px-10 py-4 font-mono text-xl font-bold gap-15 max-lg:gap-2 max-lg:px-1 max-xl:px-5 max-xl:gap-5">
             <li
-              className="hover:text-yellow-400 cursor-pointer"
+              className="cursor-pointer hover:text-yellow-400"
               onClick={(e) => setApiData("business")}
             >
               Business
             </li>
             <li
-              className="hover:text-yellow-400 cursor-pointer"
+              className="cursor-pointer hover:text-yellow-400"
               onClick={(e) => setApiData("entertainment")}
             >
               Entertainment
             </li>
             <li
-              className="hover:text-yellow-400 cursor-pointer"
+              className="cursor-pointer hover:text-yellow-400"
               onClick={(e) => setApiData("general")}
             >
               General
             </li>
             <li
-              className="hover:text-yellow-400 cursor-pointer"
+              className="cursor-pointer hover:text-yellow-400"
               onClick={(e) => setApiData("health")}
             >
               Health
             </li>
             <li
-              className="hover:text-yellow-400 cursor-pointer"
+              className="cursor-pointer hover:text-yellow-400"
               onClick={(e) => setApiData("science")}
             >
               Science
             </li>
             <li
-              className="hover:text-yellow-400 cursor-pointer"
+              className="cursor-pointer hover:text-yellow-400"
               onClick={(e) => setApiData("sports")}
             >
               Sports
             </li>
             <li
-              className="hover:text-yellow-400 cursor-pointer"
+              className="cursor-pointer hover:text-yellow-400"
               onClick={(e) => setApiData("technology")}
             >
               Technology
@@ -83,13 +94,13 @@ function NewsItems() {
         </div>
 
         {/* Mobile Navbar */}
-        <div className="md:hidden flex justify-between items-center px-4 py-4 h-20 ">
-          <div className="text-2xl font-mono text-center bg-red-700 rounded-full px-4 py-2 hover:bg-red-600 transition-all duration-300 shadow-lg shadow-yellow-500">
+        <div className="flex items-center justify-between h-20 px-4 py-4 md:hidden ">
+          <div className="px-4 py-2 font-mono text-2xl text-center transition-all duration-300 bg-red-700 rounded-full shadow-lg hover:bg-red-600 shadow-yellow-500">
             <h1 className="animate-bounce">NewSApp</h1>
           </div>
           <button
             onClick={() => setOpen(!isOpen)}
-            className="text-white text-3xl font-bold focus:outline-none"
+            className="text-3xl font-bold text-white focus:outline-none"
           >
             {isOpen ? "✕" : "☰"}
           </button>
@@ -104,79 +115,90 @@ function NewsItems() {
           }`}
         >
           <a
-            className="hover:text-yellow-400 cursor-pointer"
+            className="cursor-pointer hover:text-yellow-400"
             onClick={() => setApiData("business")}
           >
             Business
           </a>
           <a
-            className="hover:text-yellow-400 cursor-pointer"
+            className="cursor-pointer hover:text-yellow-400"
             onClick={() => setApiData("entertainment")}
           >
             Entertainment
           </a>
           <a
-            className="hover:text-yellow-400 cursor-pointer"
+            className="cursor-pointer hover:text-yellow-400"
             onClick={() => setApiData("general")}
           >
             General
           </a>
           <a
-            className="hover:text-yellow-400 cursor-pointer"
+            className="cursor-pointer hover:text-yellow-400"
             onClick={() => setApiData("health")}
           >
             Health
           </a>
           <a
-            className="hover:text-yellow-400 cursor-pointer"
+            className="cursor-pointer hover:text-yellow-400"
             onClick={() => setApiData("science")}
           >
             Science
           </a>
           <a
-            className="hover:text-yellow-400 cursor-pointer"
+            className="cursor-pointer hover:text-yellow-400"
             onClick={() => setApiData("sports")}
           >
             Sports
           </a>
           <a
-            className="hover:text-yellow-400 cursor-pointer"
+            className="cursor-pointer hover:text-yellow-400"
             onClick={() => setApiData("technology")}
           >
             Technology
           </a>
         </div>
       </div>
-
-      <div className="w-full h-auto flex flex-col justify-center items-center py-20 gap-5">
-        <div className="w-full text-white flex justify-center items-center gap-2  mt-3 h-auto
-        ">
-          <input type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Enter your Area" 
-          className="border w-1/2 h-10 rounded-full text-center text-white font-mono font-bold  border-blue-500 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+      <div className="flex flex-col items-center justify-center w-full h-auto gap-5 py-20">
+        <div
+          className="flex items-center justify-center w-full h-auto gap-2 mt-3 text-white "
+        >
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Enter your Area"
+            className="w-1/2 h-10 font-mono font-bold text-center text-white border border-blue-500 rounded-full focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
           />
-          <button className="border py-2 px-5 rounded-full bg-blue-400 shadow-md shadow-amber-100 font-bold" onClick={handlechange} >Submit</button>
+          <FaMicrophone
+            className="w-10 h-10 py-2 bg-blue-400 border rounded-full "
+            onClick={() => {
+              recognition.start();
+            }}
+          />
+          <button
+            className="px-5 py-2 font-bold bg-blue-400 border rounded-full shadow-md shadow-amber-100"
+            onClick={handlechange}
+          >
+            Submit
+          </button>
         </div>
-        <div className="text-3xl text-white font-bold text-center my-1">
+        <div className="my-1 text-3xl font-bold text-center text-white">
           Latest{" "}
-          <span className="bg-red-600 px-5 py-1 rounded-full font-mono animate-bounce">
+          <span className="px-5 py-1 font-mono bg-red-600 rounded-full shadow-md shadow-yellow-500">
             {" "}
             News
           </span>
         </div>
-        <div className="flex flex-wrap justify-center items-center w-full max-md:flex-col">
+        <div className="flex flex-wrap items-center justify-center w-full max-md:flex-col">
           {response.map((article) => {
             return (
               <NewsBox
-                key={article.article_id}
                 description={article.description || "No description available"}
-                title={article.title}
-                image_url={article.image_url}
-                read={article.link}
+                title={article.title || "No title available"}
+                image_url={article.urlToImage || "no image available"}
+                read={article.url || "#"}
               />
-            )
+            );
           })}
         </div>
       </div>
